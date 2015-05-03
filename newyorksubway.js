eb7746f5b48255ef100a2eed28651cbd2aa4216d
Daisy Molving <daisymolving@gmail.com>
2015-05-04 06:26:51 +1200

    created variables for origin and destination

--- newyorksubway.js
+++ newyorksubway.js
@@ -1,6 +1,14 @@
-var lineL = ["8thL", "6thL", "Union Square", "3rdL", "1stL"];
+
+var lineL = ["8thL", "6thL", "Union Square", "3rdL", "1stL"]; 
 var lineSix = ["Grand Central6", "33rd6", "28th6", "23rd6", "Union Square", "Astor Place6"];
 var lineN = ["Times SquareN", "34th", "28thN", "23rdN", "Union Square", "8thN"];
 
+{
+  var originStation = prompt("Please type the station that you will catch the train from:");
+  var destinationStation = prompt("Please type the station that you want to go to:");
+}
+
 
 console.log(lineL.length);
+console.log(lineSix.length);
+console.log(lineN.length);
