abcc77f073aeb9f175f1728171dd017d763808bb
Daisy Molving <daisymolving@gmail.com>
2015-05-04 00:23:40 +1200

    made string arrays of trainlines, printed length

---
+++ newyorksubway.js
@@ -0,0 +1,6 @@
+var lineL = ["8thL", "6thL", "Union Square", "3rdL", "1stL"];
+var lineSix = ["Grand Central6", "33rd6", "28th6", "23rd6", "Union Square", "Astor Place6"];
+var lineN = ["Times SquareN", "34th", "28thN", "23rdN", "Union Square", "8thN"];
+
+
+console.log(lineL.length);
