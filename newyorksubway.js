04250addd2608cb9b323b3cd495b113ef4142b4d
Daisy Molving <daisymolving@gmail.com>
2015-05-04 10:25:32 +1200

    pseudo code and nonsense

--- newyorksubway.js
+++ newyorksubway.js
@@ -1,7 +1,7 @@
 
 var lineL = ["8thL", "6thL", "Union Square", "3rdL", "1stL"]; 
 var lineSix = ["Grand Central6", "33rd6", "28th6", "23rd6", "Union Square", "Astor Place6"];
-var lineN = ["Times SquareN", "34th", "28thN", "23rdN", "Union Square", "8thN"];
+var lineN = ["Times SquareN", "34thN", "28thN", "23rdN", "Union Square", "8thN"];
 
 {
   var originStation = prompt("Please type the station that you will catch the train from:");
@@ -22,13 +22,18 @@ var lineN = ["Times SquareN", "34th", "28thN", "23rdN", "Union Square", "8thN"];
 //console.log(lineSix.length);
 //console.log(lineN.length);
 
-
+//on line L union square is at index position 2
 var resultLineL = Math.abs(lineL.indexOf(originStation) - lineL.indexOf(destinationStation)); 
 
+//on line 6 union square is at index position 4
 var resultLineSix = Math.abs(lineSix.indexOf(originStation) - lineSix.indexOf(destinationStation)); 
 
+//on line N union square is at index position 4
 var resultLineN = Math.abs(lineN.indexOf(originStation) - lineN.indexOf(destinationStation)); {
 
 alert(resultLineL + resultLineSix + resultLineN + " stops");
  
 }
+
+//if the journey does not pass through union square then 
+
